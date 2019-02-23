52d082374b6fdaaba2cc4dc1aa25b6e365baed1f
Ivo Ivanov <ivo.ivanov@evry.com>
2019-02-23 22:18:59 +0100

    posturepal

---
+++ precache-manifest.814f26fee6e32577c629c68379dc86a8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b54308c5a33606ead5a",
+    "url": "/static/css/main.64ea21f1.chunk.css"
+  },
+  {
+    "revision": "3b54308c5a33606ead5a",
+    "url": "/static/js/main.3b54308c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d5c811c40bfc6172ff39",
+    "url": "/static/js/2.d5c811c4.chunk.js"
+  },
+  {
+    "revision": "b193cb0f01ebc88243be8bc8870a5980",
+    "url": "/static/media/giveawayFigure-image.b193cb0f.png"
+  },
+  {
+    "revision": "25dcd5cd345e0d450e3e18b65ac7220f",
+    "url": "/static/media/PosturePal_logo.25dcd5cd.svg"
+  },
+  {
+    "revision": "d027343e9488e42d3d64a3edf94c08de",
+    "url": "/static/media/PosturePal_logo_text.d027343e.svg"
+  },
+  {
+    "revision": "fb148185da22b9c0e0101bc968ed5678",
+    "url": "/static/media/menu.fb148185.svg"
+  },
+  {
+    "revision": "7e4e5ddbef12dcf02118f78af51da82e",
+    "url": "/static/media/UniNeueRegular.7e4e5ddb.otf"
+  },
+  {
+    "revision": "7b99adece88b53f147f2bb7aea4762d0",
+    "url": "/static/media/UniNeueBold.7b99adec.otf"
+  },
+  {
+    "revision": "270b7d3f7356ce1a68c52efd83f1f017",
+    "url": "/static/media/UniNeueHeavy.270b7d3f.otf"
+  },
+  {
+    "revision": "1a6de4d6caaae5b411dcf2417b5a71b0",
+    "url": "/index.html"
+  }
+];
