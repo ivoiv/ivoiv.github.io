ba84dea09d364f384c6c002a4b7241a178abf55a
Ivo Ivanov <xionor@gmail.com>
2019-05-29 10:10:02 +0200

    Kahoot current

---
+++ precache-manifest.d33d1c62ebd395f64c0abfaf65ebcde0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4179dd59db5c3159720aea50e7b1bc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d3e32ca4843f8b0914",
+    "url": "/static/js/2.188d5cf5.chunk.js"
+  },
+  {
+    "revision": "1051bb184fab54893aa0",
+    "url": "/static/js/main.dd88679c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
